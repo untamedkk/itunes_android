4ab5caae2a640da1289312d77d3c58311d2469ec
Krishna Gupta <krishna.gupta3090@gmail.com>
2025-03-29 12:20:05 +0530

    some code clean up

--- app/src/main/java/com/example/itunes/di/ApiModule.kt
+++ app/src/main/java/com/example/itunes/di/ApiModule.kt
@@ -12,16 +12,8 @@ import java.util.concurrent.TimeUnit
 
 fun provideHttpClient(): OkHttpClient {
 
-    val logger = HttpLoggingInterceptor().apply {
-        this.level = HttpLoggingInterceptor.Level.BODY
-    }
-    return OkHttpClient
-        .Builder().apply {
-            this.addInterceptor(logger)
-        }
-        .readTimeout(60, TimeUnit.SECONDS)
-        .connectTimeout(60, TimeUnit.SECONDS)
-        .addInterceptor(provideLoggingInterceptor()).build()
+    return OkHttpClient.Builder().readTimeout(60, TimeUnit.SECONDS)
+        .connectTimeout(60, TimeUnit.SECONDS).addInterceptor(provideLoggingInterceptor()).build()
 }
 
 fun provideLoggingInterceptor(): HttpLoggingInterceptor {
@@ -29,24 +21,16 @@ fun provideLoggingInterceptor(): HttpLoggingInterceptor {
 }
 
 fun provideConverterFactory(): MoshiConverterFactory {
-    val moshi =
-        Moshi.Builder()
-            .addLast(KotlinJsonAdapterFactory())
-            .build()
+    val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
     return MoshiConverterFactory.create(moshi)
 }
 
 
 fun provideRetrofit(
-    okHttpClient: OkHttpClient,
-    moshiConverterFactory: MoshiConverterFactory
+    okHttpClient: OkHttpClient, moshiConverterFactory: MoshiConverterFactory
 ): Retrofit {
-    return Retrofit.Builder()
-        .baseUrl("https://itunes.apple.com/")
-        .client(okHttpClient)
-        .addConverterFactory(moshiConverterFactory)
-        .build()
+    return Retrofit.Builder().baseUrl("https://itunes.apple.com/").client(okHttpClient)
+        .addConverterFactory(moshiConverterFactory).build()
 }
 
-fun provideService(retrofit: Retrofit): ApiService =
-    retrofit.create(ApiService::class.java)
+fun provideService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
